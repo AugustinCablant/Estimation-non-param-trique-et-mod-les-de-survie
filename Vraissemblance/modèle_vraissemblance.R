512062e0cd893a2ff486cc8082654ba02efef5c0
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-08-16 14:24:41 +0200

    Update modèle_vraissemblance.R

--- Vraissemblance/modèle_vraissemblance.R
+++ Vraissemblance/modèle_vraissemblance.R
@@ -1,2 +1,18 @@
-galap <- read.csv("../donnees/galapagos.csv")
+# Apprendre à maximiser une fonction de vraissemblance : 
+
+galap <- read.csv("Vraissemblance/galapagos.csv")
 str(galap)
+
+# Tout d’abord, nous devons écrire une fonction qui calcule l’opposé de la log-vraisemblance 
+#(negative loglikelihood) pour notre problème.
+#Par convention, les algorithmes d’optimisation demandent une fonction à
+#minimiser, donc au lieu de maximiser la log-vraisemblance, on minimise son opposé.
+
+nll_galap <- function(b_0, b_area, b_near, theta) {
+mu_sp <- exp(b_0 + b_area * log(galap$Area) + b_near * log(galap$Nearest))
+-sum(dnbinom(galap$Species, mu = mu_sp, size = theta, log = TRUE))
+}
+
+library(bbmle)
+mle_galap <- mle2(nll_galap, start = list(b_0 = 0, b_area = 0, b_near = 0, theta = 1))
+mle_galap
