b6be8798235804a3b8b1d00f340a82a92b70d38a
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-08-17 09:49:12 +0200

    compute

--- Vraissemblance/vraissemblance_code.py
+++ Vraissemblance/vraissemblance_code.py
@@ -1,21 +1,25 @@
 import numpy as np 
 import pandas as pd 
+import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from scipy.integrate import quad
 import random
 from tqdm import tqdm
+from prettytable import PrettyTable
 
-seller = pd.read_csv('Data/df_vraissemblance1.csv')
-X = ['type_libre','sexe_homme','idf','etranger','une_tete','dec1','dec2','dec3']
-columns = ['type_libre','sexe_homme','idf','etranger','une_tete','dec1','dec2','dec3','tau_birth','tau_contract','Td','Ts','Td_clone','Ts_clone','tau_begin','tau_end']
+seller = pd.read_csv('Data/dataset_vraissemblance.csv')
+
+
+X = ['type_libre','sexe_homme','sexe_femme','idf','etranger','une_tete','dec1','dec2','dec3']
+columns = ['type_libre','sexe_homme','sexe_femme','idf','etranger','une_tete','dec1','dec2','dec3','tau_birth','tau_contract','Td','Ts','Td_clone','Ts_clone','tau_begin','tau_end']
 
 ### Quelques fonctions utiles ###
-def phiD(beta_d): # beta_d est un vecteur de taille 8
+def phiD(beta_d): # beta_d est un vecteur de taille 9
     x_i = seller[X].values 
     phi = np.exp(np.dot(x_i,beta_d))
     return phi / phi.mean()
 
-def phiS(beta_s): # beta_d est un vecteur de taille 8
+def phiS(beta_s): # beta_d est un vecteur de taille 9
     x_i = seller[X].values 
     phi = np.exp(np.dot(x_i,beta_s))
     return phi / phi.mean()
@@ -42,7 +46,6 @@ def LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
     numerateur_s_exp = np.exp(-(phi_s[i] * IS(lambda_s,seller['Ts'][i])))
     numerateur_s = lambda_s * phi_s[i] * numerateur_s_exp
     numerateur = numerateur_d * numerateur_s
-    
     # dénominateur 
     s = lambda_s * phi_s[i] 
     d = lambda_d * phi_d[i] 
@@ -54,6 +57,7 @@ def LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
     membre_3 = np.exp(- d * t_begin - s * t_end) - np.exp( - t_end * (d + s)) - s * (np.exp(- d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin ) - np.exp( - t_end * (s + d))) / deno    
     denominateur = membre_1 + membre_2 + membre_3
     resultat = numerateur / denominateur
+    
     return resultat
 
 
@@ -95,23 +99,22 @@ def likelihood(parameters):
     parameters = list(parameters)
     lambda_d = parameters[0]
     lambda_s = parameters[1]
-    beta_d = list(parameters[2:10])
-    beta_s = list(parameters[10:18])
+    beta_d = list(parameters[2:11])
+    beta_s = list(parameters[11:20])
     delta = parameters[-1]
 
     phi_d = phiD(beta_d)
     phi_s = phiS(beta_s)
     L_seller_sum = 0
     L_clone_sum = 0
-
+    
     for i in tqdm(seller.index.to_list()): 
         Log_seller = vlog_negatif(LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i))
         Log_clone = vlog_negatif(LClone_i(lambda_d, lambda_s, phi_d, phi_s,delta, i))
-        print(Log_seller,Log_clone)
-        L_seller_sum = L_seller_sum + Log_seller
-        L_clone_sum = L_clone_sum + Log_clone
-
-    
+        if Log_seller != np.inf and Log_clone != np.inf and Log_seller != - np.inf and Log_clone != - np.inf: 
+            if Log_seller != np.nan and Log_clone != np.nan and Log_seller != None and Log_clone != None:
+                L_seller_sum = L_seller_sum + Log_seller
+                L_clone_sum = L_clone_sum + Log_clone
     # Log_vraisemblance
     L_1 = np.sum(L_seller_sum)
     L_2 = np.sum(L_clone_sum)
@@ -129,61 +132,35 @@ seller['Ts_clone'] *= 10**-3
 seller['tau_begin'] *= 10**-3
 seller['tau_end'] *= 10**-3
 
-"""""
-### Estimation ### 
-initial_params = np.random.uniform(2,15,size=19)
 
+initial_params = np.random.uniform(-50,50,size=21)
+liste_compteur = []
 result = minimize(likelihood, initial_params, method='L-BFGS-B', options={'maxiter':1000, 'disp': True, 'ftol': 1e-1})
 
 # Résultats
 estimated_params = result.x
-print("Estimated Parameters:", estimated_params)
-print("success ? ", result.success)
-print(result.message)
+success = result.success
+message = result.message
 
+# Créer un tableau
+table = PrettyTable()
+table.field_names = ["Parameter", "Value"]
 
-print(LSeller_i(np.random.uniform(1,5,size=1),
-               np.random.uniform(1,5,size=1),
-               phiD(np.random.uniform(1,5,size=8)),
-                phiS(np.random.uniform(1,5,size=8)),
-                np.random.uniform(1,5,size=1),
-                1))
-print(LClone_i(np.random.uniform(1,5,size=1),
-               np.random.uniform(1,5,size=1),
-               phiD(np.random.uniform(1,5,size=8)),
-                phiS(np.random.uniform(1,5,size=8)),
-                np.random.uniform(1,5,size=1),
-                1))
-"""
-#print(phiD(np.random.uniform(1,5,size=8))[12] * IDD(np.random.uniform(1,5,size=1), np.random.uniform(1,5,size=1), seller['Td'][12],seller['Ts'][12]))
+# Ajouter les résultats au tableau
+for i, param in enumerate(estimated_params):
+    if i==0: 
+        table.add_row(["lambda_d", param])
+    elif i==1:
+        table.add_row(["lambda_s", param])
+    elif i in [k for k in range(2,11)]:
+        table.add_row([f"beta_d{i - 2}", param])
+    elif i in [k for k in range(11,20)]:
+        table.add_row([f"beta_s{i - 11}", param])
+    elif i==20:
+        table.add_row(["delta", param])
 
-lambda_d = np.random.uniform(2, 10, size=1) 
-lambda_s = np.random.uniform(2, 10, size=1) 
-phi_d = phiD(np.random.uniform(2, 10, size=8))
-phi_s = phiS(np.random.uniform(2, 10, size=8)) 
-delta = np.random.uniform(2, 10, size=1) 
+table.add_row(["Success", success])
+table.add_row(["Message", message])
 
-i = 340
-
-numerateur_d_exp =  np.exp(-(phi_d[i] * IDD(delta, lambda_d, seller['Td'][i], seller['Ts'][i])))
-numerateur_d = lambda_d * phi_d[i] * delta * numerateur_d_exp
-    
-numerateur_s_exp = np.exp(-(phi_s[i] * IS(lambda_s, seller['Ts'][i])))
-numerateur_s = lambda_s * phi_s[i] * numerateur_s_exp
-numerateur = numerateur_d * numerateur_s
-
-# dénominateur 
-s = lambda_s * phi_s[i]
-d = lambda_d * phi_d[i] 
-t_end = seller['tau_end'][i] - seller['tau_birth'][i]
-t_begin = seller['tau_begin'][i] - seller['tau_birth'][i]
-deno = d * (1 - delta) + s
-membre_1 = - (s * (np.exp( - d * (delta * t_end - (1 - delta) * t_begin )) - np.exp(- t_end * (d - s)))) / deno
-    
-membre_2 = np.exp(-t_end * (d + s)) - np.exp(- d * t_begin - s * t_end)
-
-membre_3 = np.exp(- d * t_begin - s * t_end) - np.exp( - t_end * (d + s)) - s * (np.exp(- d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin ) - np.exp( - t_end * (s + d))) / deno
-    
-denominateur = membre_1 + membre_2 + membre_3
-resultat = numerateur / denominateur
-print(i,resultat)
+# Afficher le tableau
+print(table)
