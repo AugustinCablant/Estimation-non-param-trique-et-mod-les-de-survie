726093ddcdc677a669cbe6ed83e7379b18c4d282
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-08-31 15:17:47 +0200

    Update vraissemblance_code.py

--- Vraisemblance/vraissemblance_code.py
+++ Vraisemblance/vraissemblance_code.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from prettytable import PrettyTable
 
 seller = pd.read_csv('Data/dataset_vraissemblance.csv')
-
+facteur_de_normalisation = 10 ** (-5)
 
 X = ['sexe_femme','idf','etranger','dec1','dec2','dec3']
 columns = ['sexe_femme','idf','etranger','dec1','dec2','dec3','tau_birth','tau_contract','Td','Ts','Td_clone','Ts_clone','tau_begin','tau_end']
@@ -17,12 +17,12 @@ columns = ['sexe_femme','idf','etranger','dec1','dec2','dec3','tau_birth','tau_c
 def phiD(beta_d): 
     x_i = seller[X].values 
     phi = np.exp(np.dot(x_i,beta_d))
-    return phi / phi.mean()
+    return phi 
 
 def phiS(beta_s): 
     x_i = seller[X].values 
     phi = np.exp(np.dot(x_i,beta_s))
-    return phi / phi.mean()
+    return phi 
 
 def IDD(delta,lambda_d,t_1,t_2): 
     if t_1 <= t_2:
@@ -49,10 +49,10 @@ def LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
     # dénominateur 
     s = lambda_s * phi_s[i] 
     d = lambda_d * phi_d[i] 
-    t_end = (seller['tau_end'][i] - seller['tau_birth'][i]) * 10 ** -6
-    t_begin = (seller['tau_begin'][i] - seller['tau_birth'][i]) * 10 ** -6
+    t_end = (seller['tau_end'][i] - seller['tau_birth'][i]) * facteur_de_normalisation
+    t_begin = (seller['tau_begin'][i] - seller['tau_birth'][i]) * facteur_de_normalisation
     deno = d * (1 - delta) + s
-    membre_1 = - (s * (np.exp( - d * (delta * t_end - (1 - delta) * t_begin )) - np.exp(- t_end * (d - s)))) / deno
+    membre_1 = - (s * (np.exp( - d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin) - np.exp(- t_begin * (d + s)))) / deno
     membre_2 = np.exp( - t_end * (d + s)) - np.exp( - d * t_begin - s * t_end)
     membre_3 = np.exp(- d * t_begin - s * t_end) - np.exp( - t_end * (d + s)) - s * (np.exp(- d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin ) - np.exp( - t_end * (s + d))) / deno    
     denominateur = membre_1 + membre_2 + membre_3
@@ -63,19 +63,18 @@ def LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
 
 def LClone_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
     # numérateur
-    numerateur_d_exp =  np.exp(-(phi_d[i] * IDD(delta,lambda_d,seller['Td_clone'][i],seller['Ts_clone'][i])))
-    numerateur_d = lambda_d * phi_d[i] * delta * numerateur_d_exp
-    numerateur_s_exp = np.exp(-(phi_s[i] * IS(lambda_s,seller['Ts_clone'][i])))
-    numerateur_s = lambda_s * phi_s[i] * numerateur_s_exp
-    numerateur = numerateur_d * numerateur_s
+    numerateur_d_exp =  np.exp(-(phi_d[i] * ID(lambda_d, seller['Td_clone'][i])))
+    numerateur_d = lambda_d * phi_d[i] * numerateur_d_exp
+    numerateur_s_exp = np.exp(-(phi_s[i] * IS(lambda_s,seller['Td_clone'][i])))
+    numerateur = numerateur_d * numerateur_s_exp
     
     # dénominateur 
     s = lambda_s * phi_s[i] 
     d = lambda_d * phi_d[i] 
-    t_end = (seller['tau_end'][i] - seller['tau_birth'][i]) * 10 ** -6
-    t_begin = (seller['tau_begin'][i] - seller['tau_birth'][i]) * 10 ** -6
+    t_end = (seller['tau_end'][i] - seller['tau_birth'][i]) * facteur_de_normalisation
+    t_begin = (seller['tau_contract'][i] - seller['tau_birth'][i]) * facteur_de_normalisation
     deno = d * (1 - delta) + s
-    membre_1 = - (s * (np.exp( - d * (delta * t_end - (1 - delta) * t_begin )) - np.exp(- t_end * (d - s)))) / deno
+    membre_1 = - (s * (np.exp( - d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin) - np.exp(- t_begin * (d + s)))) / deno
     membre_2 = np.exp( - t_end * (d + s)) - np.exp( - d * t_begin - s * t_end)
     membre_3 = np.exp(- d * t_begin - s * t_end) - np.exp( - t_end * (d + s)) - s * (np.exp(- d * (delta * t_end - (1 - delta) * t_begin ) - s * t_begin ) - np.exp( - t_end * (s + d))) / deno
     denominateur = membre_1 + membre_2 + membre_3
@@ -83,11 +82,13 @@ def LClone_i(lambda_d, lambda_s, phi_d, phi_s,delta, i):
     resultat = numerateur / denominateur
     return resultat
 
+# À supprimer in fine
+"""
 def log_negatif(x):
     if x > 0: return np.log(x)
     elif x <= 0: return -np.log(-x)
 vlog_negatif = np.vectorize(log_negatif)
-
+"""
 
 # Fonction de vraissemblance
 def likelihood(parameters):
@@ -107,6 +108,7 @@ def likelihood(parameters):
     phi_s = phiS(beta_s)
     L_seller_sum = 0
     L_clone_sum = 0
+    # Il faut 2000 observations 
     for i in tqdm(seller.index.to_list()): 
         Log_seller = vlog_negatif(LSeller_i(lambda_d, lambda_s, phi_d, phi_s,delta, i))
         Log_clone = vlog_negatif(LClone_i(lambda_d, lambda_s, phi_d, phi_s,delta, i))
@@ -119,15 +121,13 @@ def likelihood(parameters):
     L_2 = np.sum(L_clone_sum)
 
     Likelihood = L_1 + L_2
-    return -Likelihood
+    return Likelihood
 
 # Réduire l'ordre de grandeur des variables
-seller['tau_birth'] = seller['tau_birth'] / seller['tau_birth'].mean()
-seller['tau_contract'] = seller['tau_contract'] / seller['tau_contract'].mean()
-seller['Ts'] = seller['Ts'] / seller['Ts'].mean()
-seller['Td'] = seller['Td'] / seller['Td'].mean()
-seller['Td_clone'] = seller['Td_clone'] / seller['Td_clone'].mean()
-seller['Ts_clone'] = seller['Ts_clone'] / seller['Ts_clone'].mean()
+seller['Ts'] = seller['Ts'] * facteur_de_normalisation
+seller['Td'] = seller['Td'] * facteur_de_normalisation
+seller['Td_clone'] = seller['Td_clone'] * facteur_de_normalisation
+seller['Ts_clone'] = seller['Ts_clone'] * facteur_de_normalisation
 
 parameters_list = [
     "lambda_d", "lambda_s", "delta",
